8fa8a948977dd857f3f3df809e37b34e9a22ce89
n37r06u3 <n37r06u3@gmail.com>
2016-04-16 07:18:54 +0800

    add new list view

--- lists/views.py
+++ lists/views.py
@@ -9,10 +9,7 @@ def home_page(request):
     #item = Item()
     #item.text = request.POST.get('item_text', '')
     #item.save()
-    if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-        return redirect('/lists/the-only-list-in-the-world/')
+
     #else:
     #    new_item_text = ''
     return render(request, 'home.html')
@@ -20,3 +17,8 @@ def home_page(request):
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
--- superlists/urls.py
+++ superlists/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     url(r'^lists/the-only-list-in-the-world/$', views.view_list,
                 name='view_list'
          ),
+    url(r'^lists/new$', views.new_list, name='new_list'),
     #url(r'^admin/', admin.site.urls),
 ]
