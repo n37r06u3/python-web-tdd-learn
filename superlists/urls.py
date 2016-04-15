b6e3b5e5c962d53b58dea8d91db32a8806753b75
n37r06u3 <n37r06u3@gmail.com>
2016-04-15 10:33:05 +0800

    post

--- superlists/urls.py
+++ superlists/urls.py
@@ -19,6 +19,6 @@ from lists import views
 urlpatterns = [
 
     #url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^$', views.home_page, name='home'),
     #url(r'^admin/', admin.site.urls),
 ]
