1b8e4b3de717da4fca579220533b8eb2e72ebfca
n37r06u3 <n37r06u3@gmail.com>
2016-04-20 10:42:01 +0800

    move sqlite database outside of main source tree

--- superlists/settings.py
+++ superlists/settings.py
@@ -75,13 +75,13 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+print(os.path.join(BASE_DIR, '../database/db.sqlite3'))
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+         'default': {
+             'ENGINE': 'django.db.backends.sqlite3',
+             'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
+         }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
